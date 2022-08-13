9a2ecf8a963c06d43d63d7b2eccfd2873fd59d5a
Zach-EE <zachholloway.n@gmail.com>
2022-08-13 18:04:18 -0400

    added whitenoise middleware in preperation for deployment

--- book_reviewer/settings.py
+++ book_reviewer/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ik8#s8=-xx#a1(0lzyp1h3liu3g-roe_0cld+*j9krwg2)@=xu'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
